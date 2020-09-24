809ada947818dcd4265fc0ad31f0e8794d4fad02
Jang <tjk1150@kakao.com>
2020-09-24 16:01:21 +0900

    #2.2 Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-function Food({fav}){
-  return <h1>I like {fav}</h1>;
+function Food({name, picture}){
+  return(
+  <div>
+    <h1>I like {name}</h1>
+    <img src={picture} />
+  </div>
+  );
 }
+
+const foodILike = [
+  {
+    id: 1,
+    name: "Kimchi",
+    image: "http://placeimg.com/64/64/1"
+  },
+  {
+    id: 2,
+    name: "Ramen",
+    image: "http://placeimg.com/64/64/2"
+  },
+  {
+    id: 3,
+    name: "Samgyeopsal",
+    image: "http://placeimg.com/64/64/3"
+  },
+  {
+    id: 4,
+    name: "Bibimbap",
+    image: "http://placeimg.com/64/64/4"
+  },
+  {
+    id: 5,
+    name: "Kimbap",
+    image: "http://placeimg.com/64/64/5"
+  }
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello!!!!</h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="samgiopsal" />
-      <Food fav="chukumi" />
-      <Food fav="kimbap" />
+      {foodILike.map(dish => (
+        <Food id={dish.id} name={dish.name} picture={dish.image}/>
+      ))}
     </div>
   );
 }
