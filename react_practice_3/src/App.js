ec15feee3011908b2f43352439fb4cb37b11a4a4
tjk1150 <tjk1150@kakao.com>
2020-09-26 22:15:05 +0900

    #parse

--- react_practice_3/src/App.js
+++ react_practice_3/src/App.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
-class App extends React.Component{
-  render(){
-    return <div />;
-    
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map((movie) => {
+              console.log(movie);
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              );
+            })}
+      </div>
+    );
   }
 }
 
