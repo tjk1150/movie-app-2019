a7d5c20195b66319f1417fbb2d5710600ef23356
tjk1150 <tjk1150@kakao.com>
2020-09-26 23:29:39 +0900

    #영화 정보 받아오기

--- react_practice_3/src/Movie.js
+++ react_practice_3/src/Movie.js
@@ -1,8 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({ id, year, title, summary, poster }) {
-  return <h4>{title}</h4>;
+function Movie({ year, title, summary, poster, genres }) {
+  return (
+    <div className="movie">
+      <img src={poster} alt={title} title={title} />
+      <div className="movie__data">
+        <h3 className="movie__title" style={{ backgroundColor: "red" }}>
+          {title}
+        </h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie__summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
 
 Movie.protoType = {
@@ -11,6 +30,7 @@ Movie.protoType = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
