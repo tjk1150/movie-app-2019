259b234dee192721bea0d3e9009a7ffd66a0daba
tjk1150 <tjk1150@kakao.com>
2020-09-27 02:15:03 +0900

    movie_app_FINISH

---
+++ react_practice_3/src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navigation() {
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/About">About</Link>
+    </div>
+  );
+}
+
+export default Navigation;
